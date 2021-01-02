67abdc9f15b2984d7ae1bfb209c4179338d5f7e0
sabillaaa <salsabila.salsabila@students.amikom.ac.id>
2021-01-02 16:33:03 +0700

    Pertemuan2 sesi 2 desktop programming

---
+++ Pertemuan1-1.py
@@ -0,0 +1,22 @@
+# Menampilkan nama
+# Hari ini saya belajar python di
+# amcc bersama divisi desktop programming
+
+# nama = "Salsabila"
+# umur = 20
+# print(type(nama))
+# print(type(umur))
+
+
+# pertama = 'Amikom Computer Club\nDivisi Desktop Programming'
+# print(pertama)
+
+# print(3*4)
+
+# angka1 = 21
+# angka2 = 3.5
+
+# bagi = angka1//angka2
+# print(type(bagi), bagi)
+
+# print("Desktop Amcc-\n"*20)
